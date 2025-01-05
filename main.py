1da34a04a6292a590d6d7c30d8011e8188a333bd
Ashy1464032 <ashy1464032@gmail.com>
2025-01-05 06:14:18 +0000

    Edited the README file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import sys
 
+
+
+
 #gets the url from the process argument
 webhook = DiscordWebhook(url=sys.argv[1], rate_limit_retry=True)
 embed = DiscordEmbed(   #Creates the nuker embed
@@ -20,4 +23,9 @@ embed.set_image("https://media1.tenor.com/m/Y496Q1n5MosAAAAd/spooky-scary.gif")
 webhook.add_embed(embed) #Add the embed to the webhook
 
 while (True) :
-    webhook.execute() #Sends the webhook message constantly to nuke the server
+    if len(sys.argv) != 2:
+        print("Usage: Python3 main.py <Discord Webhook URL>")
+        sys.exit(0)
+    else:
+        webhook.execute() #Sends the webhook message constantly to nuke the server
+        print(len(sys.argv))
